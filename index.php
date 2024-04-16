aa85137af80f8d30c3d18a83f014ceb84c29392b
saiAjithKumar <167223344+saiAjithKumar@users.noreply.github.com>
2024-04-16 20:41:47 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,164 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ecommerce Web</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+     crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+    integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="container-fluid p-0">
+    <!--first child-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-info ">
+  <div class="container-fluid">
+    <img src="./images/Shlogo.png" alt="" class="logo">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Register</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i><sup>1</sup></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Total Price:100/-</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-light" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<!--second child-->
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-secondary ">
+    <ul class="navbar-nav me-auto">
+    <li class="nav-item">
+          <a class="nav-link" href="#">Welcome Guest</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+
+</ul>
+</nav>
+
+<!--third child-->
+<div class="bg-light">
+    <h3 class="text-center">Hidden Store</h3>
+    <p class="text-center">Communication is at the heart of e-commerce and community/</p>
+</div>
+
+
+<!--fourth child-->
+<div class="row">
+    <div class="col-md-10"></div>
+    <!--products-->
+    <div class="row"></div>
+    <div class="col-md-4 mb-2">
+        <div class="card" >
+  <img src="./images/apple.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>  
+</div>
+</div>
+        </div>
+        <div class="col-md-4 mb-2">
+        <div class="card" >
+  <img src="./images/apple.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>  
+</div>
+</div>
+        </div>
+        <div class="col-md-4 mb-2"> 
+        <div class="card">
+  <img src="./images/capsicum.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
+  </div>
+</div>
+        </div>
+
+        <div class="col-md-4 mb-2">
+        <div class="card" >
+  <img src="./images/apple.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
+</div>
+</div>
+        </div>
+
+        <div class="col-md-4 mb-2">
+        <div class="card" >
+  <img src="./images/apple.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
+</div>
+</div>
+        </div>
+
+        <div class="col-md-4 mb-2">
+        <div class="card" >
+  <img src="./images/apple.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
+</div>
+</div>
+        </div>
+
+
+
+    <div class="col-md-2">
+      <ul class="navbar-nav me-auto"></ul>
+        <li class="nav-item bg-info"></li>
+        <a href="#" class="nav-link text-light">Delivery Brand</a>
+      </div>
+    <!--sidenav-->
+</div>
+
+<!--last child-->
+<div class="footer">
+    <p>All rights reserved © - Designed by SAK-2024</p>
+</div>
+    </div> 
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+    crossorigin="anonymous"></script>
+  </body>
+</html>
